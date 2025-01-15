d2d7564141f92cbe2b973873c6a280fc3a566401
Aleksey <arekabu@yandex.com>
2025-01-15 11:42:53 +0300

    Added '<  >' construction to exception replies Word 'количество' changed to 'сумму' at exception replies.

--- extensions.py
+++ extensions.py
@@ -9,22 +9,22 @@ class CurrencyConverter:
     @staticmethod
     def get_price(base: str, quote: str, amount: str) -> float:
         if quote == base:
-            raise APIException(f'Невозможно конвертироваь одинаковые валюты {quote}.')
+            raise APIException(f'Невозможно конвертироваь одинаковые валюты < {quote} >.')
 
         try:
             quote_ticker = currencies[quote]
         except KeyError:
-            raise APIException(f'Не удалось обработать валюту {quote}')
+            raise APIException(f'Не удалось обработать валюту < {quote} >')
 
         try:
             base_ticker = currencies[base]
         except KeyError:
-            raise APIException(f'Не удалось обратобать валюту {base}')
+            raise APIException(f'Не удалось обратобать валюту < {base} >')
 
         try:
             amount_check = float(amount)
         except ValueError:
-            raise APIException(f'Не удалось обработать количество {amount}')
+            raise APIException(f'Не удалось обработать сумму < {amount} >')
 
         r = requests.get(f'https://v6.exchangerate-api.com/v6/{API_KEY}/pair/{base_ticker}/{quote_ticker}/{amount}').json()
         return round(float(r['conversion_result']), 4)
